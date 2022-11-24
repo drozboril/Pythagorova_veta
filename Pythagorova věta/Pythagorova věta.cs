80f11823a0be4826428e1d647525fbf2c0059bf8
drozboril <rozborildavid77@gmail.com>
2022-11-24 17:50:59 +0100

    Uživatel zadá delku stran a & b, program vypíše délku přepony pravoúhlého trojúhelníku

---
+++ Pythagorova věta/Pythagorova věta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte délku strany a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte délku strany b");
+            int b = int.Parse(Console.ReadLine());
+            double c = a * a + b * b;
+            c = Math.Sqrt(c);
+            Console.WriteLine("Délka přepony pravoúhlého trojúhelníku je {0}", c);
+            Console.ReadLine();
+        }
+    }
+}
